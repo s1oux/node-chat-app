2139645277ca138a04c1567dd1aa4e072b80baed
andanfe@gmail.com <andanfe@gmail.com>
2019-02-27 12:59:16 +0200

    added templates for messages with mustache

--- public/js/index.js
+++ public/js/index.js
@@ -11,25 +11,28 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('HH:mm');
-  console.log('got new message', message);
+  var template = jQuery('#message-template').html();
 
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('HH:mm');
-  console.log('new location message', message);
+  var template = jQuery('#location-message-template').html();
 
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">current location</a>');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
