d2c3d2005d7eecd7988a96d7694c24ff0fe7a9f0
andanfe@gmail.com <andanfe@gmail.com>
2019-02-27 11:33:04 +0200

    added css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,11 +33,13 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'usre',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function() {
-
+    messageTextbox.val('');
   });
 });
 
@@ -48,12 +50,19 @@ locationButton.on('click', function () {
     return alert('unable to locate in your browser');
   }
 
+  locationButton.attr('disabled', 'disabled')
+                .text('locationing...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled')
+                  .text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled')
+                  .text('Send location');
     alert('unable to fetch location');
   });
 });
